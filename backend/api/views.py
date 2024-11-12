c2c10cb71a21630dfc72b655c54f2cfaf74f28ba
Andrii Boiko Planeks <andrii.boiko@planeks.net>
2024-11-12 09:04:09 +0200

    added view to handle free_trial

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import HelloResponseSerializer, HelloRequestSerializer
 
@@ -32,3 +33,23 @@ def hello_name_view(request):
     greeting = 'Hello ' + in_serializer.validated_data['name']
     out_serializer = HelloResponseSerializer({'greeting': greeting})
     return Response(out_serializer.data, status=200)
+
+
+@api_view(['POST'])
+def free_trial_view(request):
+    first_name = request.data.get('firstName')
+    last_name = request.data.get('lastName')
+    email = request.data.get('email')
+    country_code = request.data.get('countryCode')
+    phone = request.data.get('phone')
+
+    if not all([first_name, last_name, email, country_code, phone]):
+        return Response({"message": "All fields are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+    # TODO: add logic if user have access for trial
+    # if true:
+    #     free trial pass template (figma)
+    # else:
+    #     free trial fail template (figma)
+
+    return Response({"message": "Form submitted successfully!"}, status=status.HTTP_200_OK)
