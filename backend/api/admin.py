95523b7a96976c5cdc29c560b699392ff075e0b4
Andrii Boiko Planeks <andrii.boiko@planeks.net>
2024-11-13 15:02:40 +0200

    registered a new model in admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,2 +1,20 @@
 from django.contrib import admin
+from .models import FreeTrial
 
+
+@admin.register(FreeTrial)
+class FreeTrialAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'country_code', 'phone_number', 'created_at', 'updated_at')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+    list_filter = ('created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ('user', 'first_name', 'last_name', 'email', 'country_code', 'phone_number')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
