4b6b21a80e99d59e27fec6ef684cdbbc4ef57e8c
Andrii Boiko Planeks <andrii.boiko@planeks.net>
2024-11-19 09:03:41 +0200

    added youtube video component

---
+++ frontend/src/components/YouTubeVideo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "../assets/styles/YouTubeVideo.css";
+
+const YouTubeVideo = ({ videoId }) => {
+    return (
+        <div className="video-container">
+            <iframe
+                width="100%"
+                height="100%"
+                src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&mute=1&loop=1&playlist=${videoId}&modestbranding=1&rel=0&fs=0&iv_load_policy=3&disablekb=1&cc_load_policy=0`}
+                frameBorder="0"
+                allow="autoplay; fullscreen"
+                allowFullScreen
+                title="YouTube Video"
+            ></iframe>
+        </div>
+    );
+};
+
+YouTubeVideo.propTypes = {
+    videoId: PropTypes.string.isRequired,
+};
+
+export default YouTubeVideo;
